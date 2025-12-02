0ba79f52619e389b1724ef990a3ffefc7c7ea9eb
kevin-ar-cmd-lab <otienokevino100090@gmail.com>
2025-12-02 10:46:03 +0300

    updated the header file

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -3,8 +3,6 @@ import { MobileMenu } from './MobileMenu';
 
 export const Header: FC = () => (
   <header className="flex justify-between items-center p-4 shadow-md">
-    <Logo />
-    <NavMenu />
     <MobileMenu />
   </header>
 );
