a8d85b66d670b015af97a6c0a6b34e0f294da03c
kevin-ar-cmd-lab <otienokevino100090@gmail.com>
2025-12-11 21:33:07 +0300

    uodated layout1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 'use client';
 
+import { ReactNode } from 'react';
+import '../styles/globals.css';
 import { AppProviders } from '@/context/providers';
 import { Header } from '@/components/header';
 import { Footer } from '@/components/footer';
-import '../styles/globals.css';
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
